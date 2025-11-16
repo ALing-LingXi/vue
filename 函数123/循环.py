99e09164868ad3a8c945278bec77d979fe7e90c3
ALing-LingXi <2841572483@qq.com>
2025-11-16 22:48:32 +0800

    函数123/循环.py

---
+++ 函数123/循环.py
@@ -0,0 +1,15 @@
+fruits = ['apple', 'banana', 'orange', 'grape']
+# for i in fruits:
+#     print(i)
+# for i in range(9,0,-2):
+# for i in range(0,9,2):
+#     print(i)
+# for i in range(1,10):
+#     for j in range(1,10):
+#         print(i*j)
+
+
+password =""
+while password !="kkk":
+    password=input()
+print('登录成功')
