d498b7844ef1a3f32decc4f7d9a7242ee1c72bcd
ALing-LingXi <2841572483@qq.com>
2025-11-16 22:47:01 +0800

    函数123/hello world.py

---
+++ 函数123/hello world.py
@@ -0,0 +1,7 @@
+print("hello",'world',sep=',',end='-')
+print("hello",'world',sep=',',end=' ')
+print("hello",'world',sep=',',end=' ')
+print("hello",'world',sep=',',end=' ')
+a = input("请输入值")
+print("你输入的是",a)
+
