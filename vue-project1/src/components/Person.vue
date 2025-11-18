43e210296cc6234442b2d593ec847fa22a8cf1b9
ALing-LingXi <2841572483@qq.com>
2025-11-18 21:45:39 +0800

    vue-project1/src/App.vue

---
+++ vue-project1/src/components/Person.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="person">
+  <h2>{{ name }}</h2>
+  <h2>{{ age }}</h2>
+  <button @click="changeName">改变名字</button>
+  <button @click="changeAge">改变年龄</button>
+  <button @click="checkTel">查看电话号码</button>
+</div>
+</template>
+
+<script setup name="Person1111">
+    //vue3弱化this,this是undefined
+    let name ="林希"
+    let age = 18
+    let tel="18811998282"
+    function changeName(){
+      name="林希儿"//不是动态响应所以显示数据不会变
+    }
+    function changeAge(){
+      age+=1
+    }
+    function checkTel(){
+      alert(tel)
+    }
+</script>
+
+<style>
+.person{
+  height: 500px;
+  background-color: dodgerblue;
+  box-shadow: 2px 2px ;
+  color: tomato;
+  }
+</style>
