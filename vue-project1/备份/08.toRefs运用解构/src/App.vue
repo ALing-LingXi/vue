903915b656d2be20ca418e0e10671f68018db0b6
ALing-LingXi <2841572483@qq.com>
2025-11-20 21:42:22 +0800

    vue-project1/备份/08.toRefs运用解构/src/App.vue

---
+++ vue-project1/备份/08.toRefs运用解构/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+ <h1> 你好，世界。</h1>
+  <person/>
+</div>
+</template>
+
+<script >
+import Person from './components/Person.vue';
+export default {
+  name:"App",
+  components:{Person}
+  }
+</script>
+
+<style>
+div{
+  background-color: tomato;
+  padding: 40px;
+  box-shadow: 2px 4px 10%;
+}
+</style>
