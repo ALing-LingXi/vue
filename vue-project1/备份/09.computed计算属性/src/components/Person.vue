1dadcc7932a783b7c33d49ad08c21eff061b498d
ALing-LingXi <2841572483@qq.com>
2025-11-20 21:43:10 +0800

    vue-project1/备份/09.computed计算属性/src/components/Person.vue

---
+++ vue-project1/备份/09.computed计算属性/src/components/Person.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="person">
+姓: <input type="text" v-model="fistName">
+名: <input type="text" v-model="lastName">
+全名: <input type="text" v-model="fullName"></div>
+<button @click="changeName">改名</button>
+</template>
+<script setup name="Person1111">
+import { ref ,reactive,computed} from 'vue'
+  let fistName=ref('linxi')
+  let lastName=ref('er')
+  // computed 只可读
+  let fullName = computed(
+  {
+    get(){
+       return fistName.value.slice(0,1).toUpperCase()+fistName.value.slice(1)+"-"+lastName.value
+    },
+    set(val){
+      const [n1,n2] = val.split("-")
+      fistName.value=n1
+      lastName.value=n2
+    }
+  }
+  )
+  function changeName(){
+    return fullName.value="xixi-haha"
+  }
+</script>
+<style>
+.person {
+  height: 500px;
+  background-color: dodgerblue;
+  box-shadow: 2px 2px;
+  color: tomato;
+}
+</style>
