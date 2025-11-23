1e8e87554c64918d8e11fd8e4b6b90084446e38b
ALing-LingXi <2841572483@qq.com>
2025-11-23 20:52:29 +0800

    vue-project1/备份/16.ref标签属性/src/components/Person.vue

---
+++ vue-project1/备份/16.ref标签属性/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+ <h1> 你好，世界。</h1>
+ <h2 ref="title2">哈哈，那就厉害了</h2>
+<button @click="log">打印</button>
+  <person ref="xixi"/>
+</div>
+</template>
+
+<script setup name="App">
+import { ref } from 'vue';
+  import Person from './components/Person.vue';
+   let title2=ref()
+   let xixi = ref()
+  function log(){
+    console.log(title2.value);
+    console.log(xixi);
+  }
+</script>
+
+<style>
+div{
+  background-color: tomato;
+  padding: 40px;
+  box-shadow: 2px 4px 10%;
+}
+</style>
---
+++ vue-project1/备份/16.ref标签属性/src/components/Person.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="person">
+<h2 ref="title2">我不会影响你：隶属组件</h2>
+<button @click="log">打印</button>
+</div>
+</template>
+<script setup name="Person1111">
+import {reactive,watch,watchEffect,ref} from "vue"
+  let title2=ref()
+  let a=1
+  let b=2
+  function log(){
+    console.log(title2.value);
+  }
+  defineExpose({a,b})
+</script>
+<style>
+.person {
+  height: 500px;
+  background-color: dodgerblue;
+  box-shadow: 2px 2px;
+  color: tomato;
+}
+</style>
