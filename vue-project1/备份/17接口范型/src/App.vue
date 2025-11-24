9d622ffb7a820d282e3c7f73939d0a0da5804c4b
ALing-LingXi <2841572483@qq.com>
2025-11-24 21:23:21 +0800

    vue-project1/备份/17接口范型/src/Type.ts/index.ts

---
+++ vue-project1/备份/17接口范型/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+
+  <person/>
+</div>
+</template>
+
+<script setup name="App">
+
+</script>
+
+<style>
+div{
+  background-color: tomato;
+  padding: 40px;
+  box-shadow: 2px 4px 10%;
+}
+</style>
---
+++ vue-project1/备份/17接口范型/src/Type.ts/index.ts
@@ -0,0 +1,6 @@
+export interface persontype{
+  name:string,
+  age:number,
+  id:string
+}
+export type persons = Array<persontype>
