2a2f448620ec5a31df963c23f101b4252035015b
ALing-LingXi <2841572483@qq.com>
2025-11-24 21:23:46 +0800

    vue-project1/备份/19.vue3生命周期函数/src/App.vue

---
+++ vue-project1/备份/19.vue3生命周期函数/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+    <Person a="唔嘿嘿" v-if="condition"/>
+    <!-- :list="person" -->
+  </div>
+</template>
+
+<script setup name="App" lang="ts">
+import Person from './components/Person.vue';
+import { ref } from 'vue';
+
+let condition = ref(true)
+
+</script>
+
+<style>
+div {
+  background-color: tomato;
+  padding: 40px;
+  box-shadow: 2px 4px 10%;
+}
+</style>
