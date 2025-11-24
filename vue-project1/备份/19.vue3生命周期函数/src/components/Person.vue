f1492ecb92d5aa7441b490e4751f582d399bb1a9
ALing-LingXi <2841572483@qq.com>
2025-11-24 21:23:36 +0800

    vue-project1/备份/19.vue3生命周期函数/src/components/Person.vue

---
+++ vue-project1/备份/19.vue3生命周期函数/src/components/Person.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="person">
+    <h2>{{ sum }}</h2>
+    <button @click="add">点我增加</button>
+  </div>
+</template>
+<script setup name="Person1111" lang="ts">
+import { onBeforeMount,onMounted,onBeforeUpdate,onUpdated,onBeforeUnmount,onUnmounted,ref} from 'vue';
+  console.log("创建前");
+  console.log("创建完毕");
+  let sum = ref(0)
+  function add() {
+    sum++
+  }
+  onBeforeMount(()=>{
+    console.log("挂载前");
+  })
+  onMounted(()=>{
+    console.log("挂载完毕");
+    
+  })
+  onBeforeUpdate(()=>{
+    console.log("更新前");
+  })
+  onUpdated(()=>{
+    console.log("更新完毕");
+  })
+  onBeforeUnmount(()=>{
+    console.log("卸载前");
+  })
+   onUnmounted(()=>{
+    console.log("卸载完毕");
+  })
+</script>
+<style>
+.person {
+  height: 500px;
+  background-color: dodgerblue;
+  box-shadow: 2px 2px;
+  color: tomato;
+}
+</style>
