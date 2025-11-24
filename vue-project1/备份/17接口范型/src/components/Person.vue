6c194a925addb44415c41088ae4dfde180a42ec5
ALing-LingXi <2841572483@qq.com>
2025-11-24 21:23:06 +0800

    vue-project1/备份/18.Props的使用/src/Type.ts/index.ts

---
+++ vue-project1/备份/17接口范型/src/components/Person.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="person">
+
+  </div>
+</template>
+<script setup name="Person1111" lang="ts">
+import { type persontype,type persons } from "@/Type.ts"
+let a: Array<persontype> = [
+  {
+    name: "xixi",
+    age: 18,
+    id: "wohehe"
+  },
+  {
+    name: "xihe",
+    age: 18,
+    id: "wuhaha"
+  }]
+let b:persons = [ {
+    name: "xixi",
+    age: 18,
+    id: "wohehe"
+  },
+  {
+    name: "xihe",
+    age: 18,
+    id: "wuhaha"
+  }]
+</script>
+<style>
+.person {
+  height: 500px;
+  background-color: dodgerblue;
+  box-shadow: 2px 2px;
+  color: tomato;
+}
+</style>
