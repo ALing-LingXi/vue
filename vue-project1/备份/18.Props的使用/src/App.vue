b3b81ec60db814fdc2d0682eceebf659eaf3ade7
ALing-LingXi <2841572483@qq.com>
2025-11-24 21:22:49 +0800

    vue-project1/备份/18.Props的使用/src/Type.ts/index.ts

---
+++ vue-project1/备份/18.Props的使用/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+
+    <person a="唔嘿嘿"/>
+    <!-- :list="person" -->
+  </div>
+</template>
+
+<script setup name="App" lang="ts">
+import Person from './components/Person.vue';
+import { reactive } from 'vue';
+import { type persons } from "@/Type.ts"
+
+let person = reactive < persons > ([
+  { name: "xixi", age: 19, id: "123" },
+  { name: "yueye", age: 18, id: "456" },
+  { name: "Aling", age: 17, id: "789" }])
+
+</script>
+
+<style>
+div {
+  background-color: tomato;
+  padding: 40px;
+  box-shadow: 2px 4px 10%;
+}
+</style>
---
+++ vue-project1/备份/18.Props的使用/src/Type.ts/index.ts
@@ -0,0 +1,7 @@
+export interface persontype{
+  name:string,
+  age:number,
+  id:string,
+  x?:string
+}
+export type persons = persontype[]
