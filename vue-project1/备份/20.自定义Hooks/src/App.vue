382163519da1931c86ce2be8ebb433a568de3155
ALing-LingXi <2841572483@qq.com>
2025-11-25 20:57:08 +0800

    vue-project1/备份/20.自定义Hooks/src/App.vue

---
+++ vue-project1/备份/20.自定义Hooks/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+
+    <Person/>
+
+  </div>
+</template>
+
+<script setup name="App" lang="js">
+import Person from './components/Person.vue';
+
+Person
+
+</script>
+
+<style>
+div {
+  background-color: tomato;
+  padding: 40px;
+  box-shadow: 2px 4px 10%;
+}
+img{
+  height: 100px;
+  margin-right: 10px;
+}
+</style>
