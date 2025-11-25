53fd54afdd53b589d3ec14012308ede9aaca8e2c
ALing-LingXi <2841572483@qq.com>
2025-11-25 20:57:51 +0800

    vue-project1/备份/20.自定义Hooks/src/main.ts

---
+++ vue-project1/备份/20.自定义Hooks/src/hooks/useSum.ts
@@ -0,0 +1,15 @@
+import {ref,onMounted,computed} from "vue"
+import axios from "axios"
+export default function (){
+  let sum = ref(0)
+  let bigSum = computed(()=>{
+    return sum.value*10
+  })
+  function add(){
+    sum.value+=1
+  }
+  onMounted(()=>{
+    add()
+  })
+    return {sum,add,bigSum}
+  }
