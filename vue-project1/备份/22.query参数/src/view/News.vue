4aa1ecf097583de8c93a3f77160d57033f9b302d
ALing-LingXi <2841572483@qq.com>
2025-11-26 23:16:45 +0800

    vue-project1/备份/22.query参数/src/view/News.vue

---
+++ vue-project1/备份/22.query参数/src/view/News.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="news">
+    <ul>
+      <li v-for="news in newsList" :key="news.id"> 
+       <RouterLink :to='{path:"/news/detail",
+      query:{
+        id:news.id,
+        title:news.title,
+        content:news.content
+      }
+       }' >
+        {{ news.title }}</RouterLink>
+     </li>
+    </ul>
+    <div class="content">
+      <RouterView></RouterView>
+    </div>
+  </div>
+</template>
+<script setup name="News" lang="ts">
+import { reactive } from 'vue';
+import { RouterView,RouterLink } from 'vue-router';
+let newsList = reactive([
+{id:"112",title:"好消息，好消息",content:"明天放假"},
+{id:"233",title:"一种牛逼的抗癌食物",content:"西兰花"},
+{id:"455",title:"震惊,一个能蓄力三段的战技",content:"牛逼，卡利亚"},
+{id:"567",title:"难道拉塔恩没死透？",content:"来幽影地哦，指定没你好果子吃！"}])
+</script>
+<style>
+.news{
+  background-color: tomato;
+  width: 400px;
+  height: 400px;
+}
+</style>
