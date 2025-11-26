31d10f8937914d910dd42a21c4307a599c59f65f
ALing-LingXi <2841572483@qq.com>
2025-11-26 23:15:53 +0800

    vue-project1/备份/21.初见路由/src/main.ts

---
+++ vue-project1/备份/21.初见路由/src/view/News.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="news">
+    <ul>
+      <li><a href="#">001</a></li>
+      <li><a href="#">002</a></li>
+      <li><a href="#">003</a></li>
+      <li><a href="#">004</a></li>
+    </ul>
+  </div>
+</template>
+<script>
+</script>
+<style>
+.news{
+  background-color: tomato;
+  width: 400px;
+  height: 400px;
+}
+</style>
