3f7bcc30aaefbaf0b245e7f41e9bc6daedc804ce
ALing-LingXi <2841572483@qq.com>
2025-11-27 21:15:58 +0800

    vue-project1/备份/23params参数/src/view/Home.vue

---
+++ vue-project1/备份/23params参数/src/view/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img src="http://www.atguigu.com/images/index_new/logo.png" alt="">
+  </div>
+</template>
+<script setup lang="ts" name="Home">
+// 使用 script setup 语法，TypeScript 支持
+// 组件名称为 "Home"
+</script>
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+</style>
