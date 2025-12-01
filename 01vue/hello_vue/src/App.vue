1121e3215b99e80f8188a842f59fc7b11b181596
ALing-LingXi <2841572483@qq.com>
2025-12-01 23:13:15 +0800

    01vue/hello_vue/src/App.vue

---
+++ 01vue/hello_vue/src/App.vue
@@ -0,0 +1,20 @@
+<!-- html -->
+<template>
+<div class="app">
+  你好，vue
+</div>
+</template>
+<!-- ts -->
+<script lang="ts">
+export default {
+  name:"app"
+}
+</script>
+
+<style>
+div{
+background-color: ddd;
+box-shadow: 0 0 10px;
+box-sizing: border-box;
+}
+</style>
