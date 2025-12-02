6ae8d8463737d42609243a5d3bce93a06594d031
ALing-LingXi <2841572483@qq.com>
2025-12-02 21:05:47 +0800

    vue-project1/备份/26.pinia,getters,storetoRefs/src/App.vue

---
+++ vue-project1/备份/26.pinia,getters,storetoRefs/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <count/>
+    <br>
+    <sayLove/>
+</template>
+
+<script setup>
+import count from "./components/count.vue"
+import sayLove from "./components/sayLove.vue";
+</script>
+
+<style>
+
+</style>
