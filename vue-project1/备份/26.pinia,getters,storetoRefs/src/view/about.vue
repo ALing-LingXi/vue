7b4eb4cc178fe31ca29f9fcc8b772aff29fcb5a2
ALing-LingXi <2841572483@qq.com>
2025-12-02 21:05:23 +0800

    vue-project1/备份/26.pinia,getters,storetoRefs/src/view/about.vue

---
+++ vue-project1/备份/26.pinia,getters,storetoRefs/src/view/about.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="about">
+        <h2>大家好，欢迎来到尚硅谷直播间</h2>
+    </div>
+</template>
+<script setup lang="ts" name="About">
+// 使用 script setup 语法，TypeScript 支持
+// 组件名称为 "About"
+</script>
+<style scoped>
+.about {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    color: rgb(85, 84, 84);
+    font-size: 18px;
+}
+</style>
