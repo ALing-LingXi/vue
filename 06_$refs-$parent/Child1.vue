fb3d231f3ffe6f3a0627c3010cd10e520aa8bf9b
ALing-LingXi <2841572483@qq.com>
2025-12-07 22:56:04 +0800

    06_$refs-$parent/Child1.vue

---
+++ 06_$refs-$parent/Child1.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="child1">
+		玩具：{{ toy }}
+		书:{{ book }}
+		<button @click="eatHouse($parent)">败家子</button>
+  </div>
+</template>
+
+<script setup lang="ts" name="Child1">
+import { ref } from 'vue';
+import { defineExpose } from 'vue';
+let toy = ref("手办")
+let book = ref(5)
+defineExpose({toy,book})
+function eatHouse(parent:any){
+	//reactive响应式数据
+	parent.house-=1
+}
+</script>
+
+<style scoped>
+	.child1{
+		margin-top: 20px;
+		background-color: skyblue;
+		padding: 20px;
+		border-radius: 10px;
+    box-shadow: 0 0 10px black;
+	}
+</style>
