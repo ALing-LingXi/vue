4f7bdf07c2cac71e695e741eb77ce59e97e42b17
ALing-LingXi <2841572483@qq.com>
2025-12-08 22:49:27 +0800

    04_v-model/AtguiguInput.vue

---
+++ 03_mitt/Child1.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="child1">
+		<h4>哥哥的玩具{{ car }}</h4>
+		<button @click="emitter.emit('send-toy',car)">发送</button>
+  </div>
+</template>
+
+<script setup lang="ts" name="Child1">
+import { ref } from 'vue';
+import emitter from '@/utils/emitter';
+let car = ref("特斯拉")
+
+</script>
+
+<style scoped>
+	.child1{
+		margin-top: 50px;
+		background-color: skyblue;
+		padding: 10px;
+		box-shadow: 0 0 10px black;
+		border-radius: 10px;
+	}
+	.child1 button{
+		margin-right: 10px;
+	}
+</style>
