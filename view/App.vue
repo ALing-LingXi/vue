e7d527e3ca30cd5bf808fdda483fc29079518f84
ALing-LingXi <2841572483@qq.com>
2025-12-13 23:24:33 +0800

    view/about.vue

---
+++ view/App.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="haha">
+    <h2>{{msgRef}}</h2>
+  <input type="text" v-model="msgRef">
+</div>
+</template>
+
+<script setup>
+import timeRef from "./timeRef"
+let {msgRef} = timeRef("嘻嘻",2000)
+</script>
+
+<style>
+  .haha{
+    box-shadow: 0 0 10px;
+    background-color: skyblue;
+    padding: 10px;
+  }
+</style>
