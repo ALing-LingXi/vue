a3595f38288c90052a0da750a28d65f7529f7b2a
ALing-LingXi <2841572483@qq.com>
2025-12-19 23:00:06 +0800

    其他/第二个文件/src/.gitignore

---
+++ 其他/第二个文件/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- <button @click="POWER">点我改变力量</button> -->
+  <div class="box">
+    <child/>
+  </div>
+</template>
+
+<script setup name="fist review">
+import child from './child.vue';
+</script>
+
+<style>
+.box{
+  width:800px;
+  height: 800px;
+  background-color: #888;
+}
+</style>
